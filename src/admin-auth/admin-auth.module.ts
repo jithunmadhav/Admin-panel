6ad95fe51c5a2c82d7a57bd935e3f658a53bc9d5
jithun-madhav <jithun.madhav@peteye.co.in>
2025-03-18 12:08:44 +0530

    structure changed

--- src/admin-auth/admin-auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AdminAuthController } from './admin-auth.controller';
-import { AdminAuthService } from './admin-auth.service';
-
-@Module({
-  controllers: [AdminAuthController],
-  providers: [AdminAuthService]
-})
-export class AdminAuthModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminAuthModule } from './admin-auth/admin-auth.module';
+import { DatabaseModule } from './database/database.module';
+import { UserAuthModule } from './user-auth/user-auth.module';
 
 @Module({
-  imports: [AdminAuthModule],
+  imports: [AdminAuthModule, DatabaseModule, UserAuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
